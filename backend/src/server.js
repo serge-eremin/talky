ab6347602462df3088da483f7a9bf08de809b08f
Sergey <velesinc@gmail.com>
2026-02-06 22:35:34 -0500

    rate limiting implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import "dotenv/config";
 import path from "path";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -13,6 +14,7 @@ const __dirname = path.resolve();
 const PORT = ENV.PORT || 3021;
 
 app.use(express.json()); //req.body
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);

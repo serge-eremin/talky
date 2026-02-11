c0b00445be8cbaa8c3c9790cb6c0357825f889a9
Sergey <velesinc@gmail.com>
2026-02-10 19:55:57 -0500

    login & logout implemented

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,7 +1,17 @@
+import { LogOut } from "lucide-react";
 import React from "react";
+import { useAuthStore } from "../store/useAuthStore";
 
 function ChatPage() {
-  return <div className="text-white">ChatPage</div>;
+  const { logout } = useAuthStore();
+
+  return (
+    <div className="z-10 text-white">
+      ChatPage
+      <button onClick={logout}>logout</button>
+    </div>
+  );
 }
 
 export default ChatPage;
+// w-full h-screen flex items-center justify-center

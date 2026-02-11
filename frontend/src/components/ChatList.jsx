00b9150243cdb6415ee466c1ae8f03cb5c8e187d
Sergey <velesinc@gmail.com>
2026-02-10 22:44:04 -0500

    chat page layout setup

---
+++ frontend/src/components/ChatList.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function ChatList() {
+  return <div>ChatList</div>;
+}
---
+++ frontend/src/components/ProfileHeader.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const ProfileHeader = () => {
+  return <div>ProfileHeader</div>;
+};

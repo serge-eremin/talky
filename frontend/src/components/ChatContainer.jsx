e82adb1202c602e56d215465c41494af55338cda
Sergey <velesinc@gmail.com>
2026-02-17 15:25:52 -0500

    send message func. implemented

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 import { useChatStore } from "../store/useChatStore";
 import { useAuthStore } from "../store/useAuthStore";
@@ -12,9 +12,17 @@ export const ChatContainer = () => {
     useChatStore();
   const { authUser } = useAuthStore();
 
+  const messageEndRef = useRef(null);
+
   useEffect(() => {
     getMessagesByUserId(selectedUser._id);
   }, [getMessagesByUserId, selectedUser]);
+
+  useEffect(() => {
+    if (messageEndRef.current) {
+      messageEndRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [messages]);
 
   return (
     <>
@@ -41,11 +49,17 @@ export const ChatContainer = () => {
                     ) : null}
                     {msg.text ? <p className="mt-2">{msg.text}</p> : null}
                     <p className="text-xs mt-1 opacity-75 flex items-center gap-1">
-                      {new Date(msg.createdAt).toISOString().slice(11, 16)}
+                      {/* {new Date(msg.createdAt).toISOString().slice(11, 16)} */}
+                      {new Date(msg.createdAt).toLocaleTimeString(undefined, {
+                        hour: "2-digit",
+                        minute: "2-digit",
+                      })}
                     </p>
                   </div>
                 </div>
               ))}
+              {/* 👇 scroll target */}
+              <div ref={messageEndRef} />
             </div>
           ) : areMessagesLoading ? (
             <MessagesLoadingSkeleton />
@@ -54,6 +68,7 @@ export const ChatContainer = () => {
           )}
         </div>
       </div>
+
       <MessageInput />
     </>
   );
--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -2,6 +2,7 @@ import { create } from "zustand";
 import toast from "react-hot-toast";
 
 import { axiosInstance } from "../lib/axios";
+import { useAuthStore } from "./useAuthStore";
 
 export const useChatStore = create((set, get) => ({
   allContacts: [],
@@ -57,6 +58,36 @@ export const useChatStore = create((set, get) => ({
       toast.error(error.response?.data?.message || "Something went wrong");
     } finally {
       set({ areMessagesLoading: false });
+    }
+  },
+
+  sendMessage: async (messageData) => {
+    const { selectedUser, messages } = get();
+    const { authUser } = useAuthStore.getState();
+
+    const tempId = `temp-${Date.now()}`;
+
+    const optimisticMessage = {
+      _id: tempId,
+      senderId: authUser._id,
+      receiver: selectedUser._id,
+      text: messageData.text,
+      image: messageData.image,
+      createdAt: new Date().toISOString(),
+      isoptimistic: true, // flag to identity optimistic messages (optional)
+    };
+    // immidetaly update the ui by adding the message
+    set({ messages: [...messages, optimisticMessage] });
+    try {
+      const res = await axiosInstance.post(
+        `messages/send/${selectedUser._id}`,
+        messageData,
+      );
+      set({ messages: messages.concat(res.data) });
+    } catch (error) {
+      // remove optimistic message on failure
+      set({ messages: messages });
+      toast.error(error.response?.data?.message || "Something went wrong");
     }
   },
 }));
